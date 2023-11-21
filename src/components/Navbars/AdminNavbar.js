db6bdb08ca15b75d0abe77d973be5d00a2e36246
LalitJena <jlalit2022.agnitio@gmail.com>
2023-11-21 17:49:34 +0530

    color change

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -68,7 +68,7 @@ const AdminNavbar = (props) => {
                     />
                   </span>
                   <Media className="ml-2 d-none d-lg-block">
-                    <span className="mb-0 text-sm font-weight-bold">
+                    <span className="mb-0 text-sm font-weight-bold white-text">
                       Jessica Jones
                     </span>
                   </Media>
